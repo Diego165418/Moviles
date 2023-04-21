3ddb9efc9234d6d2bf6f08f58f9229aba6e84ad5
Miranda165418 <00165418@uca.edu.sv>
2023-04-21 17:05:36 -0600

    ADD: Laboratorio 05

---
+++ Laboratorio05/app/src/main/java/com/example/laboratorio_05/Add_Movie_Fragment.kt
@@ -0,0 +1,43 @@
+package com.example.laboratorio_05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class Add_Movie_Fragment : Fragment() {
+
+    private lateinit var btnSubmit : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add__movie_, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind()
+        listeners()
+    }
+
+    private fun bind(){
+        btnSubmit = view?.findViewById(R.id.btn_submit) as Button
+    }
+
+    private fun listeners(){
+        btnSubmit.setOnClickListener{
+            it.findNavController().navigate(R.id.action_add_Movie_Fragment_to_billBoard_Fragment)
+        }
+    }
+}
