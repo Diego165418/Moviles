ca9c5ed831c9d8042699732017184a84b28d7648
Miranda165418 <00165418@uca.edu.sv>
2023-04-28 17:05:19 -0600

    ADD: Laboratorio 06

---
+++ Laboratorio06/Laboratorio05/app/src/main/java/com/example/laboratorio_05/Description_Fragment.kt
@@ -0,0 +1,11 @@
+package com.example.laboratorio_05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+const val ARG_PARAM1 = "param1"
+const val ARG_PARAM2 = "param2"
+
