e5727c612b3ddd462a3dc1a04554866d925c6762
Miranda165418 <00165418@uca.edu.sv>
2023-04-28 17:05:19 -0600

    ADD: Laboratorio 06

---
+++ Laboratorio06/Laboratorio05/app/src/main/java/com/example/laboratorio_05/ui/movie/Description_Fragment.kt
@@ -0,0 +1,41 @@
+package com.example.laboratorio_05.ui.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.laboratorio_05.ARG_PARAM1
+import com.example.laboratorio_05.ARG_PARAM2
+import com.example.laboratorio_05.R
+
+class Description_Fragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description_, container, false)
+    }
+
+    companion object {
+        fun newInstance(param1: String, param2: String) =
+            Description_Fragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
